b7f9499385daaef7187733040d836c89b80bea6e
Corey Angelus <angelusproductions@gmail.com>
2019-03-24 14:04:01 -0400

    Make location entry necessary for meetings and fix spelling error in meeting.rb

--- db/migrate/20190320031751_create_meetings.rb
+++ db/migrate/20190320031751_create_meetings.rb
@@ -5,10 +5,10 @@ class CreateMeetings < ActiveRecord::Migration[5.2]
       t.date :date, null: false
       t.time :start_time, null: false
       t.time :end_time, null: false
-      t.string :street
-      t.string :city
-      t.string :country
-      t.string :zip_code
+      t.string :street, null: false
+      t.string :city, null: false
+      t.string :country, null: false
+      t.string :zip_code, null: false
 
       t.belongs_to :makerspace
       t.belongs_to :maker, null: false
