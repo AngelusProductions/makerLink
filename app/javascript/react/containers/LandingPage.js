ba42ff0e683808f3069c61674ce60a35820055ca
Corey Angelus <angelusproductions@gmail.com>
2019-05-18 18:24:15 -0400

    add pro page, change logo

--- app/javascript/react/containers/LandingPage.js
+++ app/javascript/react/containers/LandingPage.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
-import Gallery from 'react-photo-gallery'
+import { withRouter } from 'react-router-dom';
 
-class IndexPageContainer extends Component {
+import Gallery from 'react-photo-gallery'
+import Profile from './Profile'
+
+class LandingPage extends Component {
   constructor(props) {
     super(props)
     this.state = {
       makers: []
     }
+    this.onPicClick = this.onPickClick.bind(this)
   }
 
   componentDidMount () {
@@ -15,30 +19,29 @@ class IndexPageContainer extends Component {
       .then(data => { this.setState({ makers: data })})
   }
 
+  onPickClick (e) { this.props.history.push(`makers/${e.target.id}`) }
+
   render() {
     let photos = []
     if (this.state.makers.length > 0) {
       photos = this.state.makers.map( maker => {
-        return (
-          {
+        return ({
+            id: `${maker.id}`,
             src: `${maker.profile_pic.url}`,
             width: 1,
             height: 1,
             className: "gallery-image"
-          }
-        )
+          })
       })
     }
     return (
-	     <div id="index-container">
+	     <div id="landing-page-wrapper">
           <img id="makersLinking" src="https://s3.amazonaws.com/makerlink-production/makersLinking.png" alt="makersLinking" />
           <div id="makerlink-title">MakerLink</div>
-          <div id="photo-gallery">
-            <Gallery photos={photos} />
-          </div>
+          <div id="photo-gallery"><Gallery photos={photos} onClick={this.onPicClick}/></div>
        </div>
     )
   }
 }
 
-export default IndexPageContainer
+export default withRouter(LandingPage)
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
 
+  resources :makers
+
   namespace :api do
     namespace :v1 do
       resources :makers
